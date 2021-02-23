347bc994ff1cc96b0da76d1b826843ab7a026281
Carla <karlaalvarezlopez123@gmial.com>
2021-02-23 14:34:18 +0100

    test
    Completar carpetas

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+import TestComponent from '@/components/TestComponent.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
+
+describe('TestComponent.vue', () => {
+  it('should be pass when you render slots', () => {
+    const title = '<span>Title</span>'
+    const wrapper = shallowMount(TestComponent, {
+      slots: { title: title }
+    })
+    expect(wrapper.html()).toMatch(title)
+  })
+})
