a183820c4b6c6ec868a8e0896c400097854860bc
Carla <karlaalvarezlopez123@gmial.com>
2021-02-24 15:10:33 +0100

    Rutas
    Creación de rutas

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <TestComponent/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/footer-template">Footer</router-link> |
+      <router-link to="/nav-template">Nav</router-link>
+
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import TestComponent from '@/components/TestComponent.vue';
-
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    TestComponent
-  }
-}
-</script>
 
 <style>
 #app {
@@ -27,6 +18,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
